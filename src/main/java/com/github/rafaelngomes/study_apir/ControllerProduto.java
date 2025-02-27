585188fb2ec99e083784556f3caf35e589a56f22
Rafael Nascimento <rafaelng2006@gmail.com>
2025-02-27 10:17:17 -0300

    adicionada ControllerProduto

---
+++ src/main/java/com/github/rafaelngomes/study_apir/ControllerProduto.java
@@ -0,0 +1,43 @@
+package com.github.rafaelngomes.study_apir;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("produtos") //mapear a requisição
+public class ControllerProduto {
+ 
+    @PostMapping
+    public ResponseEntity<String> create() {
+        return ResponseEntity.status(201).body("Produto cadastrado");
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> delete() {
+        return ResponseEntity.status(204).build();
+    }
+
+    @PutMapping
+    public ResponseEntity<String> update(){
+        return ResponseEntity.status(200).body("Produto atualizado");
+    }
+        
+    
+
+    @GetMapping
+    public ResponseEntity<String> find() {
+        return ResponseEntity.status(200).body("Maça");
+    }
+       
+}
+
+
+
+    
+
+
